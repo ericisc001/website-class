d3808507e118f644b212139c590881d4aa850cd2
ericisc001 <79384041+ericisc001@users.noreply.github.com>
2021-05-01 05:48:08 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,28 @@
 // dark mode button onchange call multiple functions //
-function myFunction(){
+function myFunction() {
     toggleDarkMode();
     swapText();
-}
-
-// toggle dark mode//
-function toggleDarkMode() {
-
-    var element = document.body; 
+  }
+  
+  // toggle dark mode//
+  function toggleDarkMode() {
+  
+    var element = document.body;
     element.classList.toggle("dark-mode");
-}
-
-// change dark mode button text //
-function swapText() {
+    
+    var navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("nav-dark");
+  }
+  
+  // change dark mode button text //
+  function swapText() {
     var x = document.getElementById("mode");
-    if (x.innerHTML === "DARK MODE") {
-        x.innerHTML = "LIGHT MODE";
+    if (x.innerHTML === "CHANGE TO LIGHT MODE") {
+      x.innerHTML = "CHANGE TO DARK MODE";
     } else {
-        x.innerHTML = "DARK MODE";
+      x.innerHTML = "CHANGE TO LIGHT MODE";
     }
-}
+  }
 
 
 
