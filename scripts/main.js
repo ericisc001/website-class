bd84aa97011aa4c7c1318086ffa5981ded1831f4
ericisc001 <79384041+ericisc001@users.noreply.github.com>
2021-05-01 08:23:08 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,20 @@ function myFunction() {
     
     var navbar = document.querySelector(".navbar");
     navbar.classList.toggle("nav-dark");
+
+    var nav2 = document.querySelector(".nav2");
+    nav2.classList.toggle("nav2-dark");
+
+    var nav3 = document.querySelector(".nav3");
+    nav3.classList.toggle("nav3-dark");
+
+    var nav4 = document.querySelector(".nav4");
+    nav4.classList.toggle("nav4-dark");
+
+    var nav5 = document.querySelector(".nav5");
+    nav5.classList.toggle("nav5-dark");
+
+
   }
   
   // change dark mode button text //
